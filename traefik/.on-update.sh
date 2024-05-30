9c856c2338a86ae2e7b7679908d61f0a0f8316ae
Ozeliurs <ozeliurs@gmail.com>
2024-05-31 01:41:28 +0700

    feat: remove traefik + expose port

--- traefik/.on-update.sh
+++
@@ -1,7 +0,0 @@
-# Ensure the "traefik" network exists
-docker network create traefik || true
-
-# Restart all containers
-docker compose up -d --force-recreate --remove-orphans
-
-echo "Successfully updated!"
